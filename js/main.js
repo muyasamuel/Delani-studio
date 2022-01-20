0fdecc5be6db2ccd5b4348161886e9f2659fdf74
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-01-20 17:19:41 +0300

    js on icons to display info

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
+const design = document.getElementById("design");
+const designWording = document.querySelector(".DesignWording");
 
+
+design.onclick = function(){
+    designWording.style.display = "block";  
+    design.style.display = "none";
+
+  
+}
+
+const development = document.getElementById("development");
+const developmentWording = document.querySelector(".DevelopmentWording");
+
+development.onclick = function(){
+    developmentWording.style.display = "block";
+    development.style.display = "none";
+
+
+}
+
+
+const product = document.getElementById("product");
+const productWording = document.querySelector(".ProductWording");
+
+
+product.onclick = function(){
+    productWording.style.display = "block";
+    product.style.display = "none";
+
+
+}
